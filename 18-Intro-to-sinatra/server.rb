5043eea0f21ac07e80e52cb0c199364e37b883d7
coding-admin <85723463+coding-admin@users.noreply.github.com>
2021-07-12 19:00:46 -0400

    Create server.rb

---
+++ 18-Intro-to-sinatra/server.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+
+todos = ["Go for a walk", "Drink Coffee", "Work", "Sleep"]
+
+get "/" do 
+    "<h1>Hello World</h1>"
+end
+
+
+get "/hello" do 
+    @name = "Sree"
+    erb :hello
+end
+
+
+get '/msg' do
+   redirect to('/hello') 
+end
+
+
+get "/todos" do
+    @title = "Todos"
+    @todo = todos
+    erb :task
+end
+
+get '/data/:id' do
+    @title = "Gallery"
+    num = params[:id].to_i
+    @sources = []
+    counter = 88
+        num.times do
+            @sources << "https://unsplash.it/200/200?random=#{counter}"
+            counter += 1
+        end
+    erb :pic
+end
